3c6354f5e414a12048039a9369ca3957b3f2fa71
Danilo Raulino de Liz <danilo.liz@softplan.com.br>
2017-09-05 08:33:43 -0300

    feat: Criação do componente de listagem e ajuste da página inicial
    Criado a página inícial com tabuladores responsivos e com uma melhor experiência de usuário desktop e mobile e criado um componente de listagem de livros interligada a apllicação com seus respectivos dados consumidos pela API de livros cadastrados.
    
    . Utilizado o componente de 'Tabs' do 'material-ui' para uma melhor visualização e usabilidade da aplicação.
    
    . Criado um componente responsável pela listagem dos livros e ligando ele a aplicação.
    
    . Ajuste do arquivo Index.js para suprir uma necessidade de instalaçao do material-ui

---
+++ src/components/ListBooks.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+class ListBooks extends Component {
+
+    render() {
+        return (
+            <div className="list-books-content">
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">{this.props.title}</h2>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                        {this.props.books.map((book) => (
+                            <li key={book.id}>
+                                <div className="book">
+                                <div className="book-top">
+                                    <div 
+                                        className="book-cover" 
+                                        style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.smallThumbnail + '")' }}
+                                    ></div>
+                                    <div className="book-shelf-changer">
+                                        <select>
+                                            <option value="none" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                    <div className="book-title">{book.title}</div>
+                                    {book.authors && book.authors.map((author, index) => (
+                                        <div key={index} className="book-authors">{author}</div>
+                                    ))}
+                                </div>
+                            </li>
+                        ))}
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ListBooks
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBook from './App'
 import './index.css'
+
+const App = () => (
+  <MuiThemeProvider>
+    <AppBook />
+  </MuiThemeProvider>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'))
